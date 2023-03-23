aac713bf253fa32e38f49c543d55192a1684d4ca
Jared Sewak <jaredsewak@gmail.com>
2023-03-23 16:45:10 -0600

    crypto-security

---
+++ part-1/server/controllers/auth.js
@@ -0,0 +1,45 @@
+const users = []
+const bcrypt = require('bcryptjs')
+
+module.exports = {
+    login: (req, res) => {
+      console.log('Logging In User')
+      console.log(req.body)
+
+      const { username, password } = req.body
+
+      for (let i = 0; i < users.length; i++) {
+        if (users[i].username === username && users[i].password === password) {
+          const authenticated = bcrypt.compareSync(password, users[i], passwordHash)
+          if(authenticated) {
+            let userToReturn = {...user[i]}
+            delete userToReturn.passwordHash
+            return res.status(200).send(userToReturn)
+          }
+        }
+      }
+      res.status(400).send("User not found.")
+    },
+    register: (req, res) => {
+      
+      const {username, email, firstName, lastName, password} = req.body 
+      const salt = bcrypt.genSaltSync(5) 
+      const passwordHash = bcrypt.hashSync(password, salt) 
+      let user = {
+        username,
+        email,
+        firstName,
+        lastName,
+        passwordHash
+      }
+
+      console.log('Registering User')
+      console.log(req.body)
+      
+      users.push(user)
+      let userCopy = {...user}
+      delete userCopy.passwordHash
+
+      res.status(200).send(req.body)
+    }
+}
